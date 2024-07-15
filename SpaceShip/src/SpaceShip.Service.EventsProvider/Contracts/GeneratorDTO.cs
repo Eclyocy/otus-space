aa3a0dc0dbfa017a50e2dd970cb8fad1733dce72
Ksenia Altbregen <ksanph@gmail.com>
2024-07-15 20:03:55 +0300

    fix: remove not used models

--- SpaceShip/src/SpaceShip.Service.EventsProvider/Contracts/GeneratorDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SpaceShip.Service.EventsConsumer.Contracts;
-
-public class GeneratorDTO
-{
-    public Guid Id { get; set; }
-}
--- SpaceShip/src/SpaceShip.Service.EventsProvider/Contracts/ShipDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SpaceShip.Service.EventsConsumer.Contracts;
-
-public class ShipDTO
-{
-    public Guid Id { get; set; }
-}
