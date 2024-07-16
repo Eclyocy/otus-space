1e616a93a9616e392fd53057aa3d93ca9293d52a
Eclyocy <ksanph@gmail.com>
2024-07-16 17:54:30 +0300

    chore: factor-out RabbitMQ startup config to application Startup (#13)
    Создание exchange и очередей перенесено в startup.

--- GameController/GameController.API/Startup.cs
+++ GameController/GameController.API/Startup.cs
@@ -5,6 +5,10 @@ using GameController.API.ServicesExtensions;
 using GameController.API.Validators.User;
 using GameController.Database;
 using GameController.Services;
+using GameController.Services.Settings;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 
 namespace GameController
 {
@@ -53,6 +57,8 @@ namespace GameController
             {
                 endpoints.MapControllers();
             });
+
+            SetupRabbitMQ(application.ApplicationServices.GetService<IOptions<RabbitMQSettings>>());
         }
 
         /// <summary>
@@ -82,6 +88,113 @@ namespace GameController
                     info: new() { Title = "Game Controller API", Version = "v1" });
                 options.EnableAnnotations();
             });
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Set up 'new day' events fanout exchange with two queues.
+        /// </summary>
+        private static void SetupRabbitMQ(IOptions<RabbitMQSettings> rabbitMQOptions)
+        {
+            RabbitMQSettings rabbitMQSettings = rabbitMQOptions.Value;
+
+            ConnectionFactory connectionFactory = new()
+            {
+                HostName = rabbitMQSettings.Hostname,
+                Port = rabbitMQSettings.Port,
+                UserName = rabbitMQSettings.Username,
+                Password = rabbitMQSettings.Password,
+                VirtualHost = rabbitMQSettings.VirtualHost
+            };
+
+            ExchangeDeclare(connectionFactory, rabbitMQSettings.NewDayExchangeName, "fanout");
+
+            QueueDeclareAndBind(
+                connectionFactory,
+                rabbitMQSettings.NewDayQueueNameGenerator,
+                rabbitMQSettings.NewDayExchangeName,
+                routingKey: string.Empty);
+
+            QueueDeclareAndBind(
+                connectionFactory,
+                rabbitMQSettings.NewDayQueueNameShip,
+                rabbitMQSettings.NewDayExchangeName,
+                routingKey: string.Empty);
+        }
+
+        /// <summary>
+        /// Declare exchange with name <paramref name="exchangeName"/>
+        /// and type <paramref name="exchangeType"/>
+        /// if such exchange is not declared.
+        /// </summary>
+        private static void ExchangeDeclare(
+            IConnectionFactory connectionFactory,
+            string exchangeName,
+            string exchangeType)
+        {
+            try
+            {
+                using IConnection connection = connectionFactory.CreateConnection();
+                using IModel channel = connection.CreateModel();
+
+                channel.ExchangeDeclarePassive(exchangeName);
+            }
+            catch (OperationInterruptedException)
+            {
+                using IConnection connection = connectionFactory.CreateConnection();
+                using IModel channel = connection.CreateModel();
+
+                channel.ExchangeDeclare(
+                    exchange: exchangeName,
+                    type: exchangeType,
+                    durable: true,
+                    autoDelete: false);
+            }
+        }
+
+        /// <summary>
+        /// Declare queue with name <paramref name="queueName"/>
+        /// if such queue is not declared
+        /// and bind it to exchange <paramref name="exchangeName"/>
+        /// with routing key <paramref name="routingKey"/>.
+        /// </summary>
+        private static void QueueDeclareAndBind(
+            IConnectionFactory connectionFactory,
+            string queueName,
+            string exchangeName,
+            string routingKey)
+        {
+            try
+            {
+                using IConnection connection = connectionFactory.CreateConnection();
+                using IModel channel = connection.CreateModel();
+
+                channel.QueueDeclarePassive(queueName);
+            }
+            catch (OperationInterruptedException)
+            {
+                using IConnection connection = connectionFactory.CreateConnection();
+                using IModel channel = connection.CreateModel();
+
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false);
+            }
+            finally
+            {
+                using IConnection connection = connectionFactory.CreateConnection();
+                using IModel channel = connection.CreateModel();
+
+                channel.QueueBind(
+                    queue: queueName,
+                    exchange: exchangeName,
+                    routingKey: routingKey);
+            }
         }
 
         #endregion
