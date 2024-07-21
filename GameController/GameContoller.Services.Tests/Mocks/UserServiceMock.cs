e100f6583639e590d3664a3cb30e652e0cac794e
SergeyZdanov <sergeyzdanov3000@mail.ru>
2024-07-21 23:05:47 +0300

    UnitTest_not_working

---
+++ GameController/GameContoller.Services.Tests/Mocks/UserServiceMock.cs
@@ -0,0 +1,69 @@
+﻿
+using AutoMapper;
+using GameController.Database.Interfaces;
+using GameController.Database.Models;
+using GameController.Services.Interfaces;
+using GameController.Services.Models.User;
+using Moq;
+
+namespace GameContoller.Services.Tests.Mocks
+{
+    public class UserServiceMock : IUserService
+    {
+        private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public UserServiceMock()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockMapper = new Mock<IMapper>();
+        }
+
+        public UserDto GetUser(Guid userId)
+        {
+            User? user = new User
+            {
+                Id = userId,
+                Name = "MockName",
+                PasswordHash = "qwe-erty-12ty-MOCK",
+                Sessions = [new Session { Id = new Guid(), UserId = userId, ShipId = new Guid(), GeneratorId = new Guid()}]
+            };
+
+            if (user != null)
+            {
+                _mockUserRepository.Setup(m => m.Get(userId)).Returns(user);
+            }
+            else
+            {
+                _mockUserRepository.Setup(m => m.Get(userId)).Throws<ArgumentNullException>();
+            }
+
+            return _mockMapper.Object.Map<UserDto>(user);
+        }
+
+        UserDto IUserService.CreateUser(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IUserService.DeleteUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDto IUserService.GetUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<UserDto> IUserService.GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDto IUserService.UpdateUser(Guid userId, UpdateUserDto updateUserDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameController/GameContoller.Services.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using GameContoller.Services.Tests.Mocks;
+using GameController.Database.Interfaces;
+using Moq;
+using Xunit;
+
+namespace GameContoller.Services.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Mock<IUserRepository> _userRepository;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var userId = Guid.NewGuid();
+            _userRepository.Setup(s=>s.Get(userId)).Returns(true);
+
+           
+            var mockUserService = new UserServiceMock();
+
+            // Act
+            var userDto = mockUserService.GetUser(userId);
+
+            // Assert
+            Assert.IsNotNull(userDto);
+            Assert.Equals(userId, userDto.Id);
+            Assert.Equals("MockName", userDto.Name);
+        }
+
+        [Fact]
+        public void GetUser_WithExistingUser_ReturnsUserDto()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var mockUserService = new UserServiceMock();
+
+            // Act
+            var userDto = mockUserService.GetUser(userId);
+
+            // Assert
+            Assert.IsNotNull(userDto);
+            Assert.Equals(userId, userDto.Id);
+            Assert.Equals("MockName", userDto.Name);
+        }
+    }
+}
