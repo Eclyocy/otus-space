25c028c5679c83132f35e7305963ee99a4b11a55
SergeyZdanov <sergeyzdanov3000@mail.ru>
2024-08-08 22:44:29 +0300

    unit test for GetUsers

--- GameController/GameController.Services.Tests/UserServiceTests.cs
+++ GameController/GameController.Services.Tests/UserServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Xml.Linq;
+using AutoMapper;
 using GameController.Database.Interfaces;
 using GameController.Database.Models;
 using GameController.Services.Exceptions;
@@ -83,6 +84,65 @@ namespace GameController.Services.Tests
                 _mapperMock.Verify(m => m.Map<UserDto>(It.IsAny<User>()), Times.Never);
 
                 Assert.That(_loggerMock.Invocations, Has.Count.EqualTo(2));
+            });
+        }
+
+        #endregion
+
+        #region tests for GetUsers
+
+        [Test]
+        public void GetUsers_ReturnsUserDto_WhenUserExists()
+        {
+            // Arrange
+            List<User> users = new List<User>() {new User { Id = Guid.NewGuid(), Name = "Test User", PasswordHash = "hashedPassword" } };
+
+            var userId = Guid.NewGuid();
+            var user = new User { Id = userId, Name = "Test User", PasswordHash = "hashedPassword" };
+            var userDto = new UserDto { Id = userId, Name = "Test User" };
+            _userRepositoryMock.Setup(repo => repo.GetAll()).Returns(users);
+            _mapperMock.Setup(m => m.Map<List<UserDto>>(users)).Returns(new List<UserDto>() { new UserDto { Id = userId, Name = user.Name }});
+
+            // Act
+            var result = _userService.GetUsers();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Count, Is.EqualTo(users.Count));
+                foreach (var  item in result) 
+                {
+                    Assert.That(item.Id, Is.EqualTo(userDto.Id));
+                    Assert.That(item.Name, Is.EqualTo(userDto.Name));
+                }
+
+                // Verify calls
+                _userRepositoryMock.Verify(repo => repo.GetAll(), Times.Once);
+
+                _mapperMock.Verify(m => m.Map<List<UserDto>>(users), Times.Once);
+
+                Assert.That(_loggerMock.Invocations, Has.Count.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void GetUsers_ThrowsNotFoundException_WhenUserDoesNotExist()
+        {
+            // Arrange
+            _userRepositoryMock.Setup(repo => repo.GetAll()).Returns((List<User>)null);
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<NotFoundException>(() => _userService.GetUsers());
+
+                _userRepositoryMock.Verify(repo => repo.GetAll(), Times.Once);
+                _userRepositoryMock.VerifyNoOtherCalls();
+
+                _mapperMock.Verify(m => m.Map<List<UserDto>>(It.IsAny<List<User>>()), Times.Never);
+
+                Assert.That(_loggerMock.Invocations, Has.Count.EqualTo(1));
             });
         }
 
