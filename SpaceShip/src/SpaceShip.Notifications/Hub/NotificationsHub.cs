a524af8f987ffd80be3b17fe6d2ae46f9f666c31
Dmitriy Demin <dmitriy.demin@zyfra.com>
2024-09-08 19:51:25 +0300

    feat: think about subscription to ship

--- SpaceShip/src/SpaceShip.Notifications/Hub/NotificationsHub.cs
+++ SpaceShip/src/SpaceShip.Notifications/Hub/NotificationsHub.cs
@@ -4,6 +4,20 @@ namespace SpaceShip.Notifications
 {
     public class NotificationsHub : Hub, INotificationsHub
     {
+        public async Task SubscribeToShip(Guid shipId)
+        {
+            Guid tmp = shipId;
+            var clientId = Context.ConnectionId;
+            var user = Context.User;
+        }
+
+        /// <summary>
+        /// Just test method.
+        /// Using to debug.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        [Obsolete]
         public async Task SendAsync(string message)
         {
             await Clients.All.SendAsync("Receive", message);
