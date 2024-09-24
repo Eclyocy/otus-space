ba5ab1b4c261b40686c74f41fae8ddbb13a02549
Ksenia Altbregen <ksanph@gmail.com>
2024-09-25 00:32:28 +0300

    fix: add redirect for root

--- GameUI/src/app/app.routes.ts
+++ GameUI/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UsersComponent } from './components/users/users.component';
 import { ShipComponent } from './components/ship/ship.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'users/:userId', component: UserComponent },
   { path: 'users/:userId/sessions/:sessionId/ship', component: ShipComponent }
