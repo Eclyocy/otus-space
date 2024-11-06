5d78a34100dab4f79e174abba88cadd94a0c47d1
Ksenia Altbregen <ksanph@gmail.com>
2024-11-06 17:53:01 +0300

    fix: remove obsolete route

--- GameUI/src/app/app.routes.ts
+++ GameUI/src/app/app.routes.ts
@@ -5,15 +5,10 @@ import { ShipComponent } from './components/ship/ship.component';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    children: [
-      { path: '', redirectTo: 'users', pathMatch: 'full' },
-      { path: 'users', component: UsersComponent },
-      { path: 'users/:userId', component: UserComponent },
-      { path: 'users/:userId/sessions/:sessionId/ships/:shipId', component: ShipComponent }
-    ]
-  },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:userId', component: UserComponent },
+  { path: 'users/:userId/sessions/:sessionId/ships/:shipId', component: ShipComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'users' }
 ];
