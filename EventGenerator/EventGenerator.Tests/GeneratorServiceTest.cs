82f8a7a077f58493f3319e6173bcc9c7ddd3aa4f
Ksenia Altbregen <ksanph@gmail.com>
2024-11-07 14:50:18 +0300

    fix: fix tests for Generator service

--- EventGenerator/EventGenerator.Tests/GeneratorServiceTest.cs
+++ EventGenerator/EventGenerator.Tests/GeneratorServiceTest.cs
@@ -3,6 +3,7 @@ using EventGenerator.Database.Interfaces;
 using EventGenerator.Database.Models;
 using EventGenerator.Services.Exceptions;
 using EventGenerator.Services.Interfaces;
+using EventGenerator.Services.Mappers;
 using EventGenerator.Services.Models.Generator;
 using EventGenerator.Services.Services;
 using Microsoft.Extensions.Logging;
@@ -10,64 +11,101 @@ using Moq;
 
 namespace EventGenerator.Tests
 {
+    /// <summary>
+    /// Tests for <see cref="GeneratorService"/>.
+    /// </summary>
     [TestFixture]
     public class GeneratorServiceTest
     {
         #region private fields
 
+        private readonly Guid _shipId = Guid.NewGuid();
+        private readonly Guid _generatorId = Guid.NewGuid();
+
         private GeneratorService _generatorService;
+
         private Mock<IGeneratorRepository> _generatorRepositoryMock;
         private Mock<IEventService> _eventServiceMock;
         private Mock<ILogger<GeneratorService>> _loggerGeneratorMock;
-        private Mock<IMapper> _mapperMock;
-        private readonly Guid shipId = Guid.NewGuid();
-        private readonly Guid generatorId = Guid.NewGuid();
-        private readonly int troubleCoins = new Random().Next(0, 3);
+
+        private IMapper _mapper;
 
         #endregion
 
         #region setup
-        [SetUp]
-        public void SetupGeneratorServiceTests()
+
+        /// <summary>
+        /// One-time setup.
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetup()
         {
             _generatorRepositoryMock = new Mock<IGeneratorRepository>();
             _eventServiceMock = new Mock<IEventService>();
             _loggerGeneratorMock = new Mock<ILogger<GeneratorService>>();
-            _mapperMock = new Mock<IMapper>();
-            _generatorService = new GeneratorService(_generatorRepositoryMock.Object, _eventServiceMock.Object, _loggerGeneratorMock.Object, _mapperMock.Object);
+
+            _mapper = new Mapper(
+                new MapperConfiguration(
+                    static cfg =>
+                    {
+                        cfg.AddProfile<GeneratorMapper>();
+                    }));
+
+            _generatorService = new GeneratorService(
+                _generatorRepositoryMock.Object,
+                _eventServiceMock.Object,
+                _loggerGeneratorMock.Object,
+                _mapper);
+        }
+
+        /// <summary>
+        /// Setup for every test.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            _generatorRepositoryMock.Invocations.Clear();
+            _eventServiceMock.Invocations.Clear();
+            _loggerGeneratorMock.Invocations.Clear();
         }
 
         #endregion
 
         #region tests for CreateGenerator
 
+        /// <summary>
+        /// Test that <see cref="GeneratorService.CreateGenerator"/>
+        /// creates a generator.
+        /// </summary>
         [Test]
-        public void CreateGenerator_ReturnGeneratorDto_WhenGeneratorCorrect()
+        public void Test_CreateGenerator()
         {
             // Arrange
-            var generator = new Generator { Id = generatorId, ShipId = shipId, TroubleCoins = troubleCoins };
-            var createGeneratorDto = new CreateGeneratorDto { ShipId = shipId };
-            var generatorDto = new GeneratorDto { GeneratorId = generatorId, ShipId = shipId, TroubleCoins = troubleCoins };
-            _mapperMock.Setup(m => m.Map<Generator>(createGeneratorDto)).Returns(generator);
-            _generatorRepositoryMock.Setup(repo => repo.Create(generator)).Returns(generator);
-            _mapperMock.Setup(m => m.Map<GeneratorDto>(generator)).Returns(generatorDto);
+            CreateGeneratorDto serviceRequest = new() { ShipId = _shipId };
+
+            _generatorRepositoryMock
+                .Setup(repo => repo.Create(It.IsAny<Generator>()))
+                .Returns(new Generator() { Id = _generatorId, ShipId = _shipId });
 
             // Act
-            var result = _generatorService.CreateGenerator(createGeneratorDto);
+            GeneratorDto result = _generatorService.CreateGenerator(serviceRequest);
 
             // Assert
             Assert.Multiple(() =>
             {
                 Assert.That(result, Is.Not.Null);
-                Assert.That(result, Is.EqualTo(generatorDto));
-                Assert.That(result.GeneratorId, Is.EqualTo(generatorDto.GeneratorId));
-                Assert.That(result.ShipId, Is.EqualTo(generatorDto.ShipId));
-                Assert.That(result.TroubleCoins, Is.EqualTo(generatorDto.TroubleCoins));
+                Assert.That(result.GeneratorId, Is.EqualTo(_generatorId));
+                Assert.That(result.ShipId, Is.EqualTo(serviceRequest.ShipId));
+                Assert.That(result.TroubleCoins, Is.EqualTo(0));
 
-                // Verify calls
-                _generatorRepositoryMock.Verify(repo => repo.Create(generator), Times.Once);
-                _mapperMock.Verify(m => m.Map<Generator>(createGeneratorDto), Times.Once);
-                _mapperMock.Verify(m => m.Map<GeneratorDto>(generator), Times.Once);
+                _generatorRepositoryMock.Verify(
+                    repo => repo.Create(
+                        It.Is<Generator>(g =>
+                            g.Id == Guid.Empty &&
+                            g.ShipId == _shipId &&
+                            g.TroubleCoins == 0)),
+                    Times.Once);
+                _generatorRepositoryMock.VerifyNoOtherCalls();
 
                 Assert.That(_loggerGeneratorMock.Invocations, Has.Count.EqualTo(1));
             });
@@ -77,50 +115,53 @@ namespace EventGenerator.Tests
 
         #region tests for GetGenerator
 
-        [Test]
-        public void GetGenerator_ReturnsGeneratorDto_WhenGeneratorExists()
+        /// <summary>
+        /// Test that <see cref="GeneratorService.GetGenerator"/> returns
+        /// the correctly mapped generator when it exists.
+        /// </summary>
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Test_GetGenerator_WhenGeneratorExists(int troubleCoins)
         {
             // Arrange
-            var generator = new Generator { Id = generatorId, ShipId = shipId, TroubleCoins = troubleCoins };
-            var generatorDto = new GeneratorDto { ShipId = shipId, GeneratorId = generatorId, TroubleCoins = troubleCoins };
-            _generatorRepositoryMock.Setup(repo => repo.Get(generatorId, false)).Returns(generator);
-            _mapperMock.Setup(m => m.Map<GeneratorDto>(generator)).Returns(new GeneratorDto { ShipId = generator.ShipId, GeneratorId = generatorId, TroubleCoins = generator.TroubleCoins });
+            Generator generator = new() { Id = _generatorId, ShipId = _shipId, TroubleCoins = troubleCoins };
+
+            _generatorRepositoryMock.Setup(repo => repo.Get(_generatorId, It.IsAny<bool>())).Returns(generator);
 
             // Act
-            var result = _generatorService.GetGenerator(generatorId);
+            var result = _generatorService.GetGenerator(_generatorId);
 
             // Assert
             Assert.Multiple(() =>
             {
                 Assert.That(result, Is.Not.Null);
-                Assert.That(result.GeneratorId, Is.EqualTo(generatorDto.GeneratorId));
-                Assert.That(result.ShipId, Is.EqualTo(generatorDto.ShipId));
-                Assert.That(result.TroubleCoins, Is.EqualTo(generatorDto.TroubleCoins));
+                Assert.That(result.GeneratorId, Is.EqualTo(_generatorId));
+                Assert.That(result.ShipId, Is.EqualTo(_shipId));
+                Assert.That(result.TroubleCoins, Is.EqualTo(troubleCoins));
 
-                // Verify calls
-                _generatorRepositoryMock.Verify(repo => repo.Get(generatorId, false), Times.Once);
-
-                _mapperMock.Verify(m => m.Map<GeneratorDto>(generator), Times.Once);
+                _generatorRepositoryMock.Verify(repo => repo.Get(_generatorId, It.IsAny<bool>()), Times.Once);
 
                 Assert.That(_loggerGeneratorMock.Invocations, Has.Count.EqualTo(1));
             });
         }
 
-        [Test]
-        public void GetGenerator_ThrowsNotFoundException_WhenGeneratorDoesNotExist()
+        /// <summary>
+        /// Test that <see cref="GeneratorService.GetGenerator"/>
+        /// throws when the generator is not found.
+        /// </summary>
+        public void Test_GetGenerator_WhenGeneratorNotFound()
         {
             // Arrange
-            _generatorRepositoryMock.Setup(repo => repo.Get(generatorId, false)).Returns((Generator)null);
+            _generatorRepositoryMock.Setup(repo => repo.Get(_generatorId, It.IsAny<bool>())).Returns((Generator?)null);
 
             // Act & Assert
             Assert.Multiple(() =>
             {
-                Assert.Throws<NotFoundException>(() => _generatorService.GetGenerator(generatorId));
+                Assert.Throws<NotFoundException>(() => _generatorService.GetGenerator(_generatorId));
 
-                _generatorRepositoryMock.Verify(repo => repo.Get(generatorId, false), Times.Once);
+                _generatorRepositoryMock.Verify(repo => repo.Get(_generatorId, It.IsAny<bool>()), Times.Once);
                 _generatorRepositoryMock.VerifyNoOtherCalls();
-
-                _mapperMock.Verify(m => m.Map<GeneratorDto>(It.IsAny<Generator>()), Times.Never);
 
                 Assert.That(_loggerGeneratorMock.Invocations, Has.Count.EqualTo(2));
             });
@@ -130,33 +171,57 @@ namespace EventGenerator.Tests
 
         #region tests for AddTroubleCoin
 
-        [Test]
-        public void AddTroubleCoin_ReturnsGeneratorDto_WhenGeneratorExists()
+        /// <summary>
+        /// Test that <see cref="GeneratorService.AddTroubleCoin"/> increases
+        /// the trouble coins and returns the correctly mapped generator
+        /// when it exists.
+        /// </summary>
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Test_AddTroubleCoin_WhenGeneratorExists(int troubleCoins)
         {
             // Arrange
-            var generator = new Generator { Id = generatorId, ShipId = shipId, TroubleCoins = troubleCoins };
-            var generatorDto = new GeneratorDto { ShipId = shipId, GeneratorId = generatorId, TroubleCoins = troubleCoins };
+            Generator generator = new() { Id = _generatorId, ShipId = _shipId, TroubleCoins = troubleCoins };
 
-            _generatorRepositoryMock.Setup(repo => repo.Get(generatorId, false)).Returns(generator);
+            _generatorRepositoryMock.Setup(repo => repo.Get(_generatorId, It.IsAny<bool>())).Returns(generator);
             _generatorRepositoryMock.Setup(repo => repo.Update(generator));
-            _mapperMock.Setup(m => m.Map<GeneratorDto>(generator)).Returns(new GeneratorDto { ShipId = shipId, GeneratorId = generatorId, TroubleCoins = generatorDto.TroubleCoins });
 
             // Act
-            var result = _generatorService.AddTroubleCoin(generatorId);
+            GeneratorDto result = _generatorService.AddTroubleCoin(_generatorId);
 
             // Assert
             Assert.Multiple(() =>
             {
                 Assert.That(result, Is.Not.Null);
-                Assert.That(result.TroubleCoins, Is.EqualTo(generatorDto.TroubleCoins));
+                Assert.That(result.TroubleCoins, Is.EqualTo(troubleCoins + 1));
 
-                _generatorRepositoryMock.Verify(repo => repo.Get(generatorId, false), Times.Once);
+                _generatorRepositoryMock.Verify(repo => repo.Get(_generatorId, It.IsAny<bool>()), Times.Once);
                 _generatorRepositoryMock.Verify(repo => repo.Update(generator), Times.Once);
                 _generatorRepositoryMock.VerifyNoOtherCalls();
 
-                _mapperMock.Verify(m => m.Map<GeneratorDto>(generator), Times.Once);
-
                 Assert.That(_loggerGeneratorMock.Invocations, Has.Count.EqualTo(1));
+            });
+        }
+
+        /// <summary>
+        /// Test that <see cref="GeneratorService.AddTroubleCoin"/>
+        /// throws when the generator is not found.
+        /// </summary>
+        public void Test_AddTroubleCoin_WhenGeneratorNotFound()
+        {
+            // Arrange
+            _generatorRepositoryMock.Setup(repo => repo.Get(_generatorId, It.IsAny<bool>())).Returns((Generator?)null);
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<NotFoundException>(() => _generatorService.AddTroubleCoin(_generatorId));
+
+                _generatorRepositoryMock.Verify(repo => repo.Get(_generatorId, It.IsAny<bool>()), Times.Once);
+                _generatorRepositoryMock.VerifyNoOtherCalls();
+
+                Assert.That(_loggerGeneratorMock.Invocations, Has.Count.EqualTo(2));
             });
         }
 
